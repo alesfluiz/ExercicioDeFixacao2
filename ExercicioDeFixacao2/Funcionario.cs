e3b071523aa14632f87d8546110083acb556a81d
ALESF <ALESF@LAPTOP-7JNANN11>
2023-01-01 17:48:21 -0300

    Add project files.

---
+++ ExercicioDeFixacao2/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao2
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += (SalarioBruto * (porcentagem / 100.00));
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+
+    }
+}
---
+++ ExercicioDeFixacao2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionário: " + f);
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double percentual = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(percentual);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + f);
+
+
+        }
+    }
+}
